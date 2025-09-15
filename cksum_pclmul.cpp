8b465f0692445fec8f381a54be0cf2e81141b297
Terry Golubiewski <tjgolubi@netins.net>
2025-09-15 12:58:37 -0500

    Works a little slower than expected.

--- cksum_pclmul.cpp
+++ cksum_pclmul.cpp
@@ -3,55 +3,37 @@
 #include "CrcUpdate.hpp"
 #include "CrcConsts.hpp"
 #include "Int.hpp"
+#include "Simd.hpp"
 
 #include <concepts>
 #include <type_traits>
 #include <bit>
 
-#include <x86intrin.h>
-
 using uint128_t = unsigned __int128;
 
 using U128 = tjg::Int<uint128_t, std::endian::big>;
 
-constexpr __m128i ClMulDiag(__m128i x, __m128i y) noexcept {
-  auto t = _mm_clmulepi64_si128(x, y, 0x00);
-       x = _mm_clmulepi64_si128(x, y, 0x11);
-  return x ^ t;
-} // ClMulDiag
-
 U128 do_cksum_pclmul(std::uint32_t crc, const U128* buf, size_t num)
   noexcept
 {
+  using Vec = simd::Simd<simd::uint64x2_t>;
   using C = tjg::crc::CrcConsts<32, 0x04c11db7>;
+  using simd::ClMulDiag;
 
-  const __m128i SingleK = _mm_set_epi64x(C::K128_hi, C::K128_lo);
-  const __m128i FourK   = _mm_set_epi64x(C::K512_hi, C::K512_lo);
-  const __m128i ShuffleK = _mm_set_epi8(
-                          0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
+  static const auto SingleK = Vec{C::K128_lo, C::K128_hi};
+  static const auto FourK   = Vec{C::K512_lo, C::K512_hi};
 
-  auto Load = [&](U128 x) -> __m128i {
-    if constexpr (U128::Endian == std::endian::native)
-      return (__m128i) x.value();
-    else
-      return _mm_shuffle_epi8((__m128i) x.raw(), ShuffleK);
-  };
+  auto Load   = [](U128 x) -> Vec  { return  Vec{x}; };
+  auto Unload = [](Vec  x) -> U128 { return U128{x}; };
 
-  auto Unload = [&](__m128i x) -> U128 {
-    if constexpr (U128::Endian == std::endian::native)
-      return U128::Raw((uint128_t) x);
-    else
-      return U128::Raw((uint128_t) _mm_shuffle_epi8(x, ShuffleK));
-  };
+  auto data0 = Load(buf[0]);
 
-  __m128i data0 = Load(buf[0]);
-
-  data0 ^= (__m128i) (uint128_t{crc} << (128-32));
+  data0 ^= Vec{uint128_t{crc} << (128-32)};
 
   if (num >= 8) {
-    __m128i data1 = Load(buf[1]);
-    __m128i data2 = Load(buf[2]);
-    __m128i data3 = Load(buf[3]);
+    auto data1 = Load(buf[1]);
+    auto data2 = Load(buf[2]);
+    auto data3 = Load(buf[3]);
 
     for ( ; num >= 8; num -= 4) {
       buf += 4;
